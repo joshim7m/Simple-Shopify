20c6795de559d84531687afec0128458cd130837
joshim7m <joshim7m@gmail.com>
2021-09-29 20:49:31 +0600

    Initaill Homa Page Setup

--- assets/application.js
+++ assets/application.js
@@ -1,103 +1,78 @@
-$('.btn.edit-address').click(function() {
-  $(this).hide();
-  $(this).parents('td').siblings('.address-edit').show();
-  $(this).siblings('.btn.edit-address-cancel').show();
-  $(this).siblings('.btn.edit-address-submit').show();
-  $(this).siblings('.btn.delete-address').hide();
-  $(this).parents('td').siblings('.address-record').hide()
-});
+$(document).ready(function(){
 
-$('.btn.edit-address-submit').click(function() {
-  $(this).hide();
-  $(this).parents('td').siblings('.address-edit').hide();
-  $(this).siblings('.btn.edit-address-cancel').hide();
-  $(this).siblings('.btn.edit-address').show();
-  $(this).siblings('.btn.delete-address').show();
-  $(this).parents('td').siblings('.address-record').show()
-});
+  // MOBILE MENU ==================
+  $('.menu-trigger').on('click', function(){
+    $('.mobile-nav').addClass('show');
+  })
 
-$('.btn.edit-address-cancel').click(function() {
-  $(this).hide();
-  $(this).parents('td').siblings('.address-edit').hide();
-  $(this).siblings('.btn.edit-address-submit').hide();
-  $(this).siblings('.btn.edit-address').show();
-  $(this).siblings('.btn.delete-address').show();
-  $(this).parents('td').siblings('.address-record').show()
-});
+  $('.nav-close').on('click', function(){
+    $('.mobile-nav').removeClass('show');
+  })
 
-$('.carousel').carousel()
 
-$('.single-product-images').slick({
-  slidesToShow: 1,
-  slidesToScroll: 1,
-  dots: true,
-  customPaging : function(slider, i) {
-    var thumb = $(slider.$slides[i]).data('thumb');
-    return `<a><img class="img-fluid" src=${thumb} /></a>`;
-  },
-  responsive: [
+
+  // SEARCH BOX ================
+  $('#search-trigger').on('click', function(){
+    $('.search-box').addClass('show');
+  })
+
+  $('#search-box-close').on('click', function(){
+    $('.search-box').removeClass('show');
+  })
+
+  // CART DRAWER ================
+
+  $('#cartDrawer-trigger').on('click', function(){
+    $('.cartDrawer').addClass('show');
+  })
+
+  $('#cartDrawer-close').on('click', function(){
+    $('.cartDrawer').removeClass('show');
+  })
+
+
+
+  // ON SCROLL STICKY NAV
+  var header = $("#header");
+    $(window).scroll(function() {
+        var scroll = $(window).scrollTop();
+        if (scroll >= 100 && $(this).width() > 767) {
+            header.addClass("sticky-nav");
+        } else {
+            header.removeClass('sticky-nav');
+        }
+    });   
+
+
+  // BESTSELLERS CAROUSEL
+  $('.bestsellers__row').slick({
+    dots: false,
+    arrows: true,
+    slidesToShow: 1,
+    slidesToScroll: 1,
+    rows: 2,
+    infinite: false,
+    mobileFirst: true,
+    nextArrow: '<i class="bestsellers-slide-arrow left">2</i>',
+    prevArrow: '<i class="bestsellers-slide-arrow right"t>1</i>',
+    responsive: [
     {
-      breakpoint: 1024,
+      breakpoint: 1200,
       settings: {
         slidesToShow: 3,
         slidesToScroll: 3,
-        infinite: true,
-        dots: true
       }
     },
     {
-      breakpoint: 600,
+      breakpoint: 767,
       settings: {
         slidesToShow: 2,
         slidesToScroll: 2
       }
-    },
-    {
-      breakpoint: 480,
-      settings: {
-        slidesToShow: 1,
-        slidesToScroll: 1
-      }
     }
   ]
-});
 
-function getVariantFromOptions() {
-  let variantArr = []
-  $(".product-category select").map(function(i, el) {
-    variant = {value: $(el).val(), index: $(el).data('index')};
-    variantArr.push(variant)
   });
-  return variantArr;
-}
+})
 
-function updateHistoryState(variant) {
-  if (!history.replaceState || !variant) {
-    return;
-  }
-
-  var newurl =
-    window.location.protocol +
-    '//' +
-    window.location.host +
-    window.location.pathname +
-    '?variant=' +
-    variant.id;
-  
-  window.history.replaceState({ path: newurl }, '', newurl);
-}
-
-$('.product-category select').on('change', function() {
-  var selectedValues = getVariantFromOptions();
-  var variants = window.product.variants;
-  
-  // Search for product variants based on what was selected in the dropdowns
-  var found = _.find(variants, function(variant) {
-    return selectedValues.every(function(values) {
-      return _.isEqual(variant[values.index], values.value);
-    });
-  });
-  updateHistoryState(found)
-  $('#variant-id').val(found.id)
-});
 
---
+++ assets/main.js
@@ -0,0 +1,78 @@
+$(document).ready(function(){
+
+	// MOBILE MENU ==================
+	$('.menu-trigger').on('click', function(){
+		$('.mobile-nav').addClass('show');
+	})
+
+	$('.nav-close').on('click', function(){
+		$('.mobile-nav').removeClass('show');
+	})
+
+
+
+	// SEARCH BOX ================
+	$('#search-trigger').on('click', function(){
+		$('.search-box').addClass('show');
+	})
+
+	$('#search-box-close').on('click', function(){
+		$('.search-box').removeClass('show');
+	})
+
+	// CART DRAWER ================
+
+	$('#cartDrawer-trigger').on('click', function(){
+		$('.cartDrawer').addClass('show');
+	})
+
+	$('#cartDrawer-close').on('click', function(){
+		$('.cartDrawer').removeClass('show');
+	})
+
+
+
+	// ON SCROLL STICKY NAV
+	var header = $("#header");
+    $(window).scroll(function() {
+        var scroll = $(window).scrollTop();
+        if (scroll >= 100 && $(this).width() > 767) {
+            header.addClass("sticky-nav");
+        } else {
+            header.removeClass('sticky-nav');
+        }
+    });	  
+
+
+	// BESTSELLERS CAROUSEL
+	$('.bestsellers__row').slick({
+	  dots: false,
+	  arrows: true,
+	  slidesToShow: 1,
+	  slidesToScroll: 1,
+	  rows: 2,
+	  infinite: false,
+	  mobileFirst: true,
+	  nextArrow: '<i class="bestsellers-slide-arrow left">2</i>',
+	  prevArrow: '<i class="bestsellers-slide-arrow right"t>1</i>',
+	  responsive: [
+    {
+      breakpoint: 1200,
+      settings: {
+        slidesToShow: 3,
+        slidesToScroll: 3,
+      }
+    },
+    {
+      breakpoint: 767,
+      settings: {
+        slidesToShow: 2,
+        slidesToScroll: 2
+      }
+    }
+  ]
+
+	});
+})
+
+
